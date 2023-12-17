861f6a72fdb48f8245e791c22905c3b17976feca
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-17 18:33:19 +0100

    Added ScavTrap

---
+++ ex02/ScavTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 15:59:10 by abenamar          #+#    #+#             */
+/*   Updated: 2023/12/17 16:53:13 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __SCAVTRAP_HPP__
+#define __SCAVTRAP_HPP__
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap();
+	ScavTrap(std::string name);
+	ScavTrap(ScavTrap const &src);
+	~ScavTrap();
+
+	ScavTrap &operator=(ScavTrap const &rhs);
+
+	void attack(const std::string &target); /* override */
+
+	void guardGate();
+};
+
+#endif
