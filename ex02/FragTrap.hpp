7ee8b6678a68d8e0c52919b71c370e2bc240ecd2
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-17 18:33:24 +0100

    Added FragTrap

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 17:42:32 by abenamar          #+#    #+#             */
+/*   Updated: 2023/12/17 17:48:15 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FRAGTRAP_HPP__
+#define __FRAGTRAP_HPP__
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+	FragTrap();
+	FragTrap(std::string name);
+	FragTrap(FragTrap const &src);
+	~FragTrap();
+
+	FragTrap &operator=(FragTrap const &rhs);
+
+	void highFivesGuys(void);
+};
+
+#endif
