ec29432b051614d976167797bdff32e64ce4a7a6
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-17 18:33:15 +0100

    Added ClapTrap

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 12:07:35 by abenamar          #+#    #+#             */
+/*   Updated: 2023/12/17 16:41:33 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CLAPTRAP_HPP__
+#define __CLAPTRAP_HPP__
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const &src);
+	~ClapTrap();
+
+	ClapTrap &operator=(ClapTrap const &rhs);
+
+	std::string getName(void) const;
+	void setName(std::string name);
+	unsigned int getHitPoints(void) const;
+	void setHitPoints(unsigned int hitPoints);
+	unsigned int getEnergyPoints(void) const;
+	void setEnergyPoints(unsigned int energyPoints);
+	unsigned int getAttackDamage(void) const;
+	void setAttackDamage(unsigned int attackDamage);
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+protected:
+	bool hitsAndEnergyPointsCheck(void) const;
+	void consumeEnergyPoints(unsigned int amount);
+
+private:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+};
+
+std::ostream &operator<<(std::ostream &o, ClapTrap const &i);
+
+#endif
