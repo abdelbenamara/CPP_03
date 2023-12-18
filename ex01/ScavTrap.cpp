bd567fc8e815b5d82a40b7c6c67ddec753f26a6b
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-18 14:34:04 +0100

    Updated ScavTrap

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 16:11:45 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/17 17:32:26 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/18 14:32:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,7 @@ ScavTrap::~ScavTrap()
 
 ScavTrap &ScavTrap::operator=(ScavTrap const &rhs)
 {
-	if (this != &rhs)
-	{
-		this->setName(rhs.getName());
-		this->setHitPoints(rhs.getHitPoints());
-		this->setEnergyPoints(rhs.getEnergyPoints());
-		this->setAttackDamage(rhs.getAttackDamage());
-	}
+	ClapTrap::operator=(rhs);
 
 	return *this;
 }
