cc9f18c5060b6432f7a5a676bde987b885cbf685
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-22 18:42:15 +0100

    Updated DiamondTrap

--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -6,13 +6,14 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 16:02:03 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/22 18:26:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/22 18:40:24 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __DIAMONDTRAP_HPP__
 #define __DIAMONDTRAP_HPP__
 
+#include <iostream>
 #include <string>
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
