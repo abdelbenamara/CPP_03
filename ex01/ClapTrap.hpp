0102b6f93a10d09d43958bd655e00887904bc869
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-22 18:34:30 +0100

    Updated ClapTrap

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 12:07:35 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/18 15:14:33 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/22 16:33:14 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,28 @@ class ClapTrap
 {
 public:
 	ClapTrap(void);
-	ClapTrap(std::string name);
+	ClapTrap(std::string const &name);
 	ClapTrap(ClapTrap const &src);
 	~ClapTrap(void);
 
 	ClapTrap &operator=(ClapTrap const &rhs);
 
-	std::string getName(void) const;
-	void setName(std::string name);
+	std::string const &getName(void) const;
 	unsigned int getHitPoints(void) const;
-	void setHitPoints(unsigned int hitPoints);
 	unsigned int getEnergyPoints(void) const;
-	void setEnergyPoints(unsigned int energyPoints);
 	unsigned int getAttackDamage(void) const;
-	void setAttackDamage(unsigned int attackDamage);
 	void attack(const std::string &target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
 
 protected:
-	bool hitsAndEnergyPointsCheck(void) const;
-	void consumeEnergyPoints(unsigned int amount);
-
-private:
 	std::string name;
 	unsigned int hitPoints;
 	unsigned int energyPoints;
 	unsigned int attackDamage;
+
+	bool hitsAndEnergyPointsCheck(void) const;
+	void consumeEnergyPoints(unsigned int const amount);
 };
 
 std::ostream &operator<<(std::ostream &o, ClapTrap const &i);
