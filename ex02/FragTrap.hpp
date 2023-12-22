ee7591687704754b480ccc138aca22430638bbca
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-22 18:35:24 +0100

    Updated FragTrap

--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -6,20 +6,22 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:42:32 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/18 15:26:36 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/22 16:35:02 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __FRAGTRAP_HPP__
 #define __FRAGTRAP_HPP__
 
+#include <iostream>
+#include <string>
 #include "ClapTrap.hpp"
 
 class FragTrap : public ClapTrap
 {
 public:
 	FragTrap(void);
-	FragTrap(std::string name);
+	FragTrap(std::string const &name);
 	FragTrap(FragTrap const &src);
 	~FragTrap(void);
 
